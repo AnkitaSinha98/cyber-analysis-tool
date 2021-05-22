768ecea95c05e86ee9ceeb400195d4345dd1625b
Soumadeep Dhar <nicksouma@gmail.com>
2021-05-22 19:15:01 +0530

    Added Home Page animation

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,15 @@
 import React ,{useState} from 'react';
 import SimpleForm from './SimpleForm';
+import "../src/style.css";
+import gsap from "gsap";
 
+
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 
   const Home = (props) => {
     let [showChat, setShowChat] = useState(false);
@@ -8,13 +17,12 @@ import SimpleForm from './SimpleForm';
     const startChat = () => { setShowChat(true); }
     const hideChat = () => { setShowChat(false); }
       
-  
     return(
       <div>
+     <div className="Appx">
+     <div className="text-8xl pt-20 text-white-100 font-bold cursive">Welcome !!</div>
 
-      <div className="Appx">
-      <div className="text-8xl text-white-100 font-bold cursive ">Welcome !! </div>
-      </div>
+     </div>
       <div className = "bot">
         <div style ={{display: showChat ? "" : "none"}}>
           <SimpleForm></SimpleForm>
